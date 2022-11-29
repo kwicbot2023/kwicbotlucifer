0f9ba5bb66ec7f21204fb10335a296ba8f02049f
kwicbot2023 <115975818+kwicbot2023@users.noreply.github.com>
2022-11-29 23:22:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,25 +12,25 @@ def is_enabled(value, default):
 
 
 # Creator
-CREATOR_NAME = os.environ.get("CREATOR_NAME", "𝙼𝚄𝙷𝙰𝙼𝙼𝙴𝙳")
-CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "PR0FESS0_99")
+CREATOR_NAME = os.environ.get("CREATOR_NAME", "sharun")
+CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "kwic2002")
 
 # Account
-API_HASH = os.environ.get("API_HASH", "5b1d0992294a67cb54512a4fafeb0c88")
-API_ID = os.environ.get("API_ID", "6170803")
+API_HASH = os.environ.get("API_HASH", "d7b9e677664d2c87db4c4c0205b6e18f")
+API_ID = os.environ.get("API_ID", "16929529")
 # About Bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "1905228806:AAH2iahJcg5J6bqpcW11jB9KajwHbIjaslGTY")
-PICS = os.environ.get("PICS", "https://telegra.ph/file/034d53b5ed1d920ecab8b.jpg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5556181975:AAFEM7f5WsXhJK-ivXiWPXnkKDhPMJIcsas")
+PICS = os.environ.get("PICS", "https://te.legra.ph/file/8dfe7256883cbc0190478.jpg")
 # Database
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "LuciferMoringstar-Robot")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://{Username}:{Passs}@cluster0.{clusterID}.mongodb.net/myFirstDatabase?retryWrites=true{iD}=majority")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "𝙆𝙒𝙄𝘾𝘽𝙊𝙏")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://kwicbot:kwicbot@cluster0.mtrrmfd.mongodb.net/?retryWrites=true&w=majority")
 # Chats & Users
-ADMINS = os.environ.get("ADMINS", "2028425293 1637186875")
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Mo_Tech_Group")
-AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001685151224")
-CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001784382279").split()]
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001590063851")
-GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001570208190")
+ADMINS = os.environ.get("ADMINS", "942099709")
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "kwicbotupdates")
+AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001769691298")
+CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001686640034").split()]
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001620413958")
+GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001480651413")
 FILTER_DEL_SECOND = int(os.environ.get("FILTER_DEL_SECOND", "600"))
 
 # AutoFilter
